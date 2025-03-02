b78922615312155b036f9d85477b746115157246
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-03-02 19:42:09 +0100

    create the logic for creating a new note inside CreateArea.jsx

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const CreateArea = ({ onAdd }) => {
+  const [note, setNote] = useState({ title: "", content: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNote((prevNote) => ({ ...prevNote, [name]: value }));
+  };
+
+  const submitNote = (e) => {
+    e.preventDefault();
+    if (note.title.trim() || note.content.trim()) {
+      onAdd(note);
+      setNote({ title: "", content: "" });
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
+        <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateArea;
