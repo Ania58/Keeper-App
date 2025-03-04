28fa62112bfc42b7f593027ce37fe5361858acca
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-03-04 15:06:31 +0100

    import delete icon from icons material and use it in the delete button instead of the plain text

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Note = ({ id, title, content, onDelete }) => {
     return (
       <div className="note">
         <h1>{title}</h1>
         <p>{content}</p>
-        {onDelete && <button onClick={() => onDelete(id)}>DELETE</button>}
+        {onDelete && <button onClick={() => onDelete(id)}><DeleteIcon /></button>}
       </div>
     );
   };
